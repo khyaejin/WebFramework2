7978118af8e322b6481afb27d4b359a84a223f74
khyaejin <olivia019182@naver.com>
2025-04-20 21:10:41 +0900

    ✨ Feat : 수강 신청 - Course 유효성 검사 어노테이션 추가

--- src/main/java/kr/ac/hansung/cse/course/model/Course.java
+++ src/main/java/kr/ac/hansung/cse/course/model/Course.java
@@ -1,6 +1,10 @@
 package kr.ac.hansung.cse.course.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,14 +19,27 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Min(value = 1973, message = "년도는 1973년 이상이어야 합니다.")
     private int year;
+
+    @Min(value = 1, message = "학기는 1 또는 2이어야 합니다.")
+    @Max(value = 2, message = "학기는 1 또는 2이어야 합니다.")
     private int semester;
+
+    @NotEmpty(message = "교과목 코드는 필수입니다.")
     private String code;
+
+    @NotEmpty(message = "교과목명은 필수입니다.")
     private String name;
 
     @Enumerated(EnumType.STRING)
+    @NotNull(message = "교과구분을 선택해주세요.")
     private CourseType type;
 
+    @NotEmpty(message = "담당 교수명을 입력해주세요.")
     private String instructor;
+
+    @Min(value = 1, message = "학점은 1 이상이어야 합니다.")
+    @Max(value = 3, message = "학점은 3 이하여야 합니다.")
     private int credit;
 }
